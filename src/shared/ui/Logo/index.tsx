9073a287ae31086cbff308d8c933391f297b842a
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2023-04-17 07:42:59 +0300

    fix: add logofilter

--- src/shared/ui/Logo/index.tsx
+++ src/shared/ui/Logo/index.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import cn from "classnames";
 
 import logomark from "./logomark.svg";
+import logofilter from "./logofilter.svg";
 
 import styles from "./styles.module.css";
 
@@ -13,7 +14,14 @@ interface Props {
 export const Logo: FC<Props> = ({ className, text }) => {
   return (
     <div className={cn(styles.root, className)}>
-      <img className={styles.logomark} src={logomark} alt="MBrello logo" />
+      <div className={styles.logo}>
+        <img className={styles.logomark} src={logomark} alt="MBrello logo" />
+        <img
+          className={styles.logofilter}
+          src={logofilter}
+          alt="MBrello logo"
+        />
+      </div>
       {text && <span className={styles.text}>{text}</span>}
     </div>
   );
