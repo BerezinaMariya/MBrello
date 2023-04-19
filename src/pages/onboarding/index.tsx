0ab5a70da28f9de6053ff241003e7eb33e89aae2
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2023-04-19 11:58:44 +0300

    fix: delete useless div from onboarding component

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -12,37 +12,35 @@ interface Props {
 
 const UserInfoPage: FC<Props> = ({ className }) => {
   return (
-    <div className={cn(styles.root, className)}>
-      <section className={styles.content}>
-        <h1 className={styles.headLine}>Please, introduce yourself</h1>
-        <p className={styles.description}>
-          You can do this later on Profile page.
-        </p>
-        <a className={styles.link} href="index.html">
-          Skip
-        </a>
-        <form className={styles.form} action="#">
-          <Input
-            className={styles.firstNameInput}
-            type="text"
-            name="firstName"
-            placeholder="First name"
-            required
-            text="First name"
-          />
-          <Input
-            className={styles.lastNameInput}
-            type="text"
-            name="lastName"
-            placeholder="Last name"
-            text="Last name"
-          />
-          <Button className={styles.button} type="submit">
-            Continue
-          </Button>
-        </form>
-      </section>
-    </div>
+    <main className={cn(styles.root, className)}>
+      <h1 className={styles.headLine}>Please, introduce yourself</h1>
+      <p className={styles.description}>
+        You can do this later on Profile page.
+      </p>
+      <a className={styles.link} href="index.html">
+        Skip
+      </a>
+      <form className={styles.form} action="#">
+        <Input
+          className={styles.firstNameInput}
+          type="text"
+          name="firstName"
+          placeholder="First name"
+          required
+          text="First name"
+        />
+        <Input
+          className={styles.lastNameInput}
+          type="text"
+          name="lastName"
+          placeholder="Last name"
+          text="Last name"
+        />
+        <Button className={styles.button} type="submit">
+          Continue
+        </Button>
+      </form>
+    </main>
   );
 };
 
