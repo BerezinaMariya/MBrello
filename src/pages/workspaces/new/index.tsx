1bd70921b028204ed6aca66efd7665a017d3582a
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2023-04-20 19:30:43 +0300

    feat: add workspaces new page

--- src/pages/workspaces/new/index.tsx
+++ src/pages/workspaces/new/index.tsx
@@ -3,6 +3,7 @@ import cn from "classnames";
 
 import { Input } from "@/shared/ui/Input";
 import { Button } from "@/shared/ui/Button";
+import { MessageChatIcon, FacebookIcon, TwitterIcon } from "@/shared/ui/Icons";
 
 import styles from "./styles.module.css";
 
@@ -10,40 +11,60 @@ interface Props {
   className?: string;
 }
 
-const OnboardingPage: FC<Props> = ({ className }) => {
+const WorkspacesNewPage: FC<Props> = ({ className }) => {
   return (
     <main className={cn(styles.root, className)}>
-      <h1 className={styles.headLine}>Please, introduce yourself</h1>
-      <div className={styles.description}>
-        <p className={styles.descriptionText}>
-          You can do this later on Profile page.
+      <section className={styles.content}>
+        <h1 className={styles.headLine}>Let's build a Workspace</h1>
+        <p className={styles.description}>
+          Boost your productivity by making it easier for everyone to access
+          boards in one location.
         </p>
-        <a className={styles.skipLink} href="/">
-          Skip
+        <form className={styles.form} action="#">
+          <Input
+            className={styles.workspaceNameInput}
+            name="WorkspaceName"
+            placeholder="Your Company Co."
+            required
+            label="Workspace name"
+            variant="md"
+          />
+          <Input
+            className={styles.descriptionInput}
+            name="Description"
+            placeholder="Our team organizes everything here."
+            label="Description"
+            variant="md"
+          />
+          <Button className={styles.button} type="submit">
+            Get started
+          </Button>
+        </form>
+      </section>
+      <section className={styles.contacts}>
+        <h2 className={styles.subHeadLine}>Get in touch</h2>
+        <p className={styles.supportingText}>
+          We’d love to hear from you. Our friendly team is always here to chat.
+        </p>
+        <h3 className={styles.chatHeadLine}>
+          <MessageChatIcon className={styles.messageChatIcon} />
+          Chat to us
+        </h3>
+        <p className={styles.chatText}>Our friendly team is here to help.</p>
+        <a
+          className={styles.link}
+          href="mailto:help@mbrello.com"
+          rel="noreferrer"
+        >
+          help@mbrello.com
         </a>
-      </div>
-      <form className={styles.form} action="workspaces/new#">
-        <Input
-          className={styles.firstNameInput}
-          name="FirstName"
-          placeholder="First name"
-          required
-          label="First name"
-          variant="md"
-        />
-        <Input
-          className={styles.lastNameInput}
-          name="LastName"
-          placeholder="Last name"
-          label="Last name"
-          variant="md"
-        />
-        <Button className={styles.button} type="submit">
-          Continue
-        </Button>
-      </form>
+        <nav className={styles.navIcons}>
+          <FacebookIcon className={styles.icon} />
+          <TwitterIcon className={styles.icon} />
+        </nav>
+      </section>
     </main>
   );
 };
 
-export default OnboardingPage;
+export default WorkspacesNewPage;
