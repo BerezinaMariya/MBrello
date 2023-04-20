a866d6e76a45ab35bc5fdeeb7bdc5465a4039869
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2023-04-20 19:30:43 +0300

    fix: add workspaces new page

--- src/shared/ui/Icons/index.ts
+++ src/shared/ui/Icons/index.ts
@@ -1,3 +1,6 @@
 import { ReactComponent as Envelope } from "./envelope.svg";
+import { ReactComponent as MessageChatIcon } from "./message-chat-icon.svg";
+import { ReactComponent as FacebookIcon } from "./facebook-icon.svg";
+import { ReactComponent as TwitterIcon } from "./twitter-icon.svg";
 
-export { Envelope };
+export { Envelope, MessageChatIcon, FacebookIcon, TwitterIcon };
