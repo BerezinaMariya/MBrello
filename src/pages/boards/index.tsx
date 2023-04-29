1a7731c95cdae95ec9706515b39e3f7087202682
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2023-04-29 12:54:50 +0300

    feat: add boards desktop

--- src/pages/boards/index.tsx
+++ src/pages/boards/index.tsx
@@ -1,7 +1,7 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import cn from "classnames";
 
-import { Logo } from "@/shared/ui/Logo";
+import { Header } from "@/shared/ui/Header";
 import { Button } from "@/shared/ui/Button";
 import {
   AvatarRingIcon,
@@ -18,61 +18,61 @@ interface Props {
 }
 
 const BoardsPage: FC<Props> = ({ className }) => {
-  const [isNavigationMenuOpen, setNavigationMenuOpen] = useState(false);
-
   return (
     <div className={cn(styles.root, className)}>
-      <header className={styles.header}>
-        <Logo className={styles.headerLogo} text={"MBrello"} />
-        <button
-          className={cn(styles.navMenuButton, styles.navMenuButtonOpen)}
-          type="button"
-          onClick={(e) => {
-            e.currentTarget.classList.toggle(styles.navMenuButtonOpen);
-            e.currentTarget.classList.toggle(styles.navMenuButtonClose);
-            setNavigationMenuOpen(!isNavigationMenuOpen);
-            console.log(e.currentTarget.classList);
-          }}
-        ></button>
-      </header>
+      <Header />
       <main className={styles.content}>
         <div className={styles.userArea}>
-          <AvatarRingIcon className={styles.userAvatarRingIcon}>
-            <p className={styles.userInitials}>BM</p>
-          </AvatarRingIcon>
           <div className={styles.infoArea}>
-            <p className={styles.userName}>Berezina Mariya Alexeevna</p>
-            <p className={styles.userAccountStatus}>Private</p>
+            <AvatarRingIcon className={styles.userAvatarRingIcon}>
+              <p className={styles.userInitials}>BM</p>
+            </AvatarRingIcon>
+            <p className={styles.userName}>
+              Berezina Mariya Alexeevna
+              <p className={styles.userAccountStatus}>Private</p>
+            </p>
+          </div>
+          <div className={styles.buttonsArea}>
+            <Button
+              className={styles.settingsButton}
+              type="button"
+              variant="gray"
+              size="sm"
+            >
+              <SettingsIcon className={styles.buttonsIcon} />
+              Settings
+            </Button>
+            <Button
+              className={styles.inviteMembersButton}
+              type="button"
+              size="sm"
+            >
+              <InviteMembersIcon className={styles.buttonsIcon} />
+              Invite members
+            </Button>
           </div>
         </div>
-        <div className={styles.buttonArea}>
-          <Button
-            className={styles.settingsButton}
-            type="button"
-            variant="gray"
-            size="sm"
-          >
-            <SettingsIcon className={styles.settingsIcon} />
-            Settings
-          </Button>
-          <Button
-            className={styles.inviteMembersButton}
-            type="button"
-            size="sm"
-          >
-            <InviteMembersIcon className={styles.inviteMembersIcon} />
-            Invite members
-          </Button>
+        <div className={styles.boardsArea}>
+          <FeaturedIcon className={styles.featuredIcon} />
+          <h3 className={styles.newBoardHeadLine}>Start by creating a board</h3>
+          <p className={styles.newBoardText}>
+            Your boards will live here. Start creating by clicking on «New
+            board»
+          </p>
+          <div className={styles.boardButtonsArea}>
+            <Button
+              className={styles.learnMoreButton}
+              type="button"
+              variant="gray"
+            >
+              Learn more
+            </Button>
+            <Button className={styles.newBoardButton} type="button">
+              <PlusIcon className={styles.plusIcon} />
+              New board
+            </Button>
+          </div>
         </div>
-        <FeaturedIcon className={styles.featuredIcon} />
-        <h3 className={styles.newBoardHeadLine}>Start by creating a board</h3>
-        <p className={styles.newBoardText}>
-          Your boards will live here. Start creating by clicking on «New board»
-        </p>
-        <Button className={styles.newBoardButton} type="button">
-          <PlusIcon className={styles.plusIcon} />
-          New board
-        </Button>
       </main>
     </div>
   );
--- src/shared/ui/Icons/index.ts
+++ src/shared/ui/Icons/index.ts
@@ -3,6 +3,10 @@ import { ReactComponent as EnvelopeIcon } from "./envelope-icon.svg";
 import { ReactComponent as MessageChatIcon } from "./message-chat-icon.svg";
 import { ReactComponent as FacebookIcon } from "./facebook-icon.svg";
 import { ReactComponent as TwitterIcon } from "./twitter-icon.svg";
+import { ReactComponent as BoardsIcon } from "./boards-icon.svg";
+import { ReactComponent as MembersIcon } from "./members-icon.svg";
+import { ReactComponent as BellIcon } from "./bell-icon.svg";
+import { ReactComponent as AvatarIcon } from "./avatar-icon.svg";
 import { ReactComponent as AvatarRingIcon } from "./avatar-ring-icon.svg";
 import { ReactComponent as SettingsIcon } from "./settings-icon.svg";
 import { ReactComponent as InviteMembersIcon } from "./invite-members-icon.svg";
@@ -15,6 +19,10 @@ export {
   MessageChatIcon,
   FacebookIcon,
   TwitterIcon,
+  BoardsIcon,
+  MembersIcon,
+  BellIcon,
+  AvatarIcon,
   AvatarRingIcon,
   SettingsIcon,
   InviteMembersIcon,
