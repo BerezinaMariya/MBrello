eae5aa3ce404c09f9f97eb36a6ce3cf534ef7d7f
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2023-05-02 16:03:38 +0300

    fix: move BoardsBackgrounds to randomBackgroundImage

--- src/entities/board/lib/randomBackgroundImage/index.ts
+++ src/entities/board/lib/randomBackgroundImage/index.ts
@@ -1,4 +1,16 @@
-import { BoardsBackgrounds } from "@/entities/board/lib/BoardsBackgrounds";
+import blueFlowersUrl from "./blue-flowers.jpg";
+import purpleFlowersUrl from "./purple-flowers.jpg";
+import redFlowersUrl from "./red-flowers.jpg";
+import whiteFlowersUrl from "./white-flowers.jpg";
+import linesUrl from "./lines.jpg";
+
+const BoardsBackgrounds = [
+  blueFlowersUrl,
+  purpleFlowersUrl,
+  redFlowersUrl,
+  whiteFlowersUrl,
+  linesUrl,
+];
 
 export function randomBackgroundImage() {
   const randomBackground = Math.floor(Math.random() * BoardsBackgrounds.length);
