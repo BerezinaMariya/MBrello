40d12094e69024dec83baca8c393b53196640cf6
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2023-05-02 16:07:30 +0300

    fix: delete console.log from randomBackgroundImage

--- src/entities/board/lib/randomBackgroundImage/index.ts
+++ src/entities/board/lib/randomBackgroundImage/index.ts
@@ -14,6 +14,5 @@ const BoardsBackgrounds = [
 
 export function randomBackgroundImage() {
   const randomBackground = Math.floor(Math.random() * BoardsBackgrounds.length);
-  console.log(BoardsBackgrounds[randomBackground]);
   return BoardsBackgrounds[randomBackground];
 }
