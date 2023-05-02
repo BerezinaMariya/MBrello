bfd42d2f210eae028a1a8a9de4463293b3bbc123
Mariya Berezina <BerezinaMariya.spb@yandex.ru>
2023-05-02 14:58:16 +0300

    fix: moved backgrounds from src/shared/ui/BoardsBackgrouns to src/ entities/board/libBoardsBackgrouns, fix bacgrounds import

---
+++ src/entities/board/lib/BoardsBackgrounds/index.ts
@@ -0,0 +1,13 @@
+import blueFlowersUrl from "./blue-flowers.jpg";
+import purpleFlowersUrl from "./purple-flowers.jpg";
+import redFlowersUrl from "./red-flowers.jpg";
+import whiteFlowersUrl from "./white-flowers.jpg";
+import linesUrl from "./lines.jpg";
+
+export const BoardsBackgrounds = [
+  blueFlowersUrl,
+  purpleFlowersUrl,
+  redFlowersUrl,
+  whiteFlowersUrl,
+  linesUrl,
+];
--- src/shared/ui/BoardsBackgrounds/index.ts
+++
@@ -1,7 +0,0 @@
-export const BoardsBackgrounds = [
-  "src/shared/ui/BoardsBackgrounds/blue-flowers.jpg",
-  "src/shared/ui/BoardsBackgrounds/purple-flowers.jpg",
-  "src/shared/ui/BoardsBackgrounds/red-flowers.jpg",
-  "src/shared/ui/BoardsBackgrounds/white-flowers.jpg",
-  "src/shared/ui/BoardsBackgrounds/lines.jpg",
-];
